b56529725ab2c1be863a94953c83e61ab37a3f46
devthiagosw <thiagosartoriweirich@hotmail.com>
2025-03-25 08:51:30 -0300

    Criação dos componentes!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/layout/login/login.component';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { MetasComponent } from './components/metas/metas.component';
+import { ContasComponent } from './components/contas/contas.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { OrcamentosComponent } from './components/orcamentos/orcamentos.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { TransacaoComponent } from './components/transacao/transacao.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginComponent },
+    { path: "", redirectTo: "login", pathMatch: "full" },
+    {
+      path: "admin",
+      component: PrincipalComponent,
+      children: [
+        { path: "dashboard", component: DashboardComponent  },
+        { path: "app-metas", component: MetasComponent },
+        { path: "contas", component: ContasComponent },
+        { path: "categorias", component: CategoriasComponent },  
+        { path: "orcamentos", component: OrcamentosComponent },
+        { path: "tags", component: TagsComponent },
+        { path: "transacao", component: TransacaoComponent },
+        { path: "categorias", component: CategoriasComponent},
+      ]
+    },
+    // wildcard DEPOIS das demais rotas
+    // { path: "**", redirectTo: "login", pathMatch: "full" }
+  ];
---
+++ src/app/components/transacao/transacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './transacao.component.html',
+  styleUrl: './transacao.component.scss'
+})
+export class TransacaoComponent {
+
+}
