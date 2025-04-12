408cfb86129e32a0fb42c18be7033c55162916a7
Barujz <carlosgbaruja@gmail.com>
2025-04-11 22:22:48 -0300

    Crud_Categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { MetasListComponent } from './components/metas/metas-list/metas-list.component';
 import { ContasListComponent } from './components/contas/conta-list/conta-list.component';
-import { CategoriasComponent } from './components/categorias/categorias.component';
+import { CategoriasListComponent } from './components/categorias/categorias-list/categorias-list.component';
 import { OrcamentosComponent } from './components/orcamentos/orcamentos.component';
 import { TagsComponent } from './components/tags/tags.component';
 import { TransacaoComponent } from './components/transacao/transacao.component';
@@ -19,11 +19,11 @@ export const routes: Routes = [
         { path: "dashboard", component: DashboardComponent  },
         { path: "app-metas", component: MetasListComponent },
         {path:"conta", component: ContasListComponent},
-        { path: "categorias", component: CategoriasComponent },  
+        { path: 'categorias', component: CategoriasListComponent },
         { path: "orcamentos", component: OrcamentosComponent },
         { path: "tags", component: TagsComponent },
         { path: "transacao", component: TransacaoComponent },
-        { path: "categorias", component: CategoriasComponent},
+        
       ]
     },
     // wildcard DEPOIS das demais rotas
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { appConfig } from './app/app.config'; // 🟢 importar isso é crucial
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
-    // ...outros providers, se houver...
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
